09f5f7dcefd1e2d6ebaccad3537919eb3a274fd5
Kseniya Timoshchuk <kseni12121@gmail.com>
2022-11-13 00:10:25 +0200

    creating the Header

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            {/* i need 5 buttons: home, search, video, message, profile  */}
+            im bottom navbar and i do nothing
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/Components/ProfileStories/ProfileStories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProfileStories = () => {
+    return (
+        <div>
+            {/* i need to be about 5 pictures of people to look like stories */}
+            i am a lot of profiles
+        </div>
+    );
+};
+
+export default ProfileStories;
