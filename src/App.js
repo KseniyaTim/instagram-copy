1261188dd496f977ca4de33e2a38b3a1fa69591d
Kseniya Timoshchuk <kseni12121@gmail.com>
2022-11-16 21:05:36 +0200

    finshing touches to comment section

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import Navbar from './Components/Navbar/Navbar';
 import Header from './Components/Header/Header';
 import './App.css';
-import ProfileStories from './Components/ProfileStories/ProfileStories';
 import Center from './Components/Center/Center';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <ProfileStories/>
+      
       <Center/>
       <Navbar/>
     </div>
--- src/Components/Center/Center.js
+++ src/Components/Center/Center.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Center.css'
 import CenterHead from './CenterHead/CenterHead';
-import CommentSection from './CommentSection/CommentSection';
+import ProfileStories from '../ProfileStories/ProfileStories'
 
 const Center = () => {
     return (
-        <div id='centerContainer'>
+        <div className='center_container center_container::-webkit-scrollbar'>
+            <ProfileStories/>
             <CenterHead/>
-            
         </div>
     );
 };
